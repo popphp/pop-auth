01055d9663c8b48856d57afe613b31dd95392358
Nick Sagona <nick@nolainteractive.com>
2015-02-11 14:20:57 -0600

    Update copyright

--- src/Adapter/Ldap.php
+++ src/Adapter/Ldap.php
@@ -6,7 +6,7 @@
  * @category   Pop
  * @package    Pop_Auth
  * @author     Nick Sagona, III <dev@nolainteractive.com>
- * @copyright  Copyright (c) 2009-2014 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @copyright  Copyright (c) 2009-2015 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
  */
 
@@ -22,7 +22,7 @@ namespace Pop\Auth\Adapter;
  * @category   Pop
  * @package    Pop_Auth
  * @author     Nick Sagona, III <dev@nolainteractive.com>
- * @copyright  Copyright (c) 2009-2014 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @copyright  Copyright (c) 2009-2015 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
  * @version    2.0.0a
  */
