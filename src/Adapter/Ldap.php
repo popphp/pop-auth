9c720a55ca6ae791eaf84adbad96279b99e5aa70
Nick Sagona <nick@nolainteractive.com>
2015-07-09 18:16:00 -0500

    Update version, add gitattributes

--- src/Adapter/Ldap.php
+++ src/Adapter/Ldap.php
@@ -24,7 +24,7 @@ namespace Pop\Auth\Adapter;
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2015 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
- * @version    2.0.0a
+ * @version    2.0.0
  */
 class Ldap extends AbstractAdapter
 {
